bd1cf0ddd20e9ee55b4815346246dc6460cdf278
Joe Stanton <joe.stanton@red-badger.com>
2014-03-09 15:41:55 +0000

    Added a validate command

--- lighthouse-agent.rb
+++ lighthouse-agent.rb
@@ -13,6 +13,16 @@ Bundler.setup
 class LighthouseAgent < Thor
   VERSION = 0.1
 
+  desc :validate, "Validates a system definition"
+  def validate(system)
+    load_system(system)
+    puts "Valid".green
+  rescue Errno::ENOENT => e
+    puts "File does not exist.".red
+  rescue => e
+    puts "Invalid : #{e.message}".red
+  end
+
   no_commands do
     def load_system(system)
       @system = DSL.load system
