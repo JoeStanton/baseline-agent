d92ffa8ec1377a0d6ffd6a056af66149c08b5457
Joe Stanton <joe.stanton@red-badger.com>
2014-03-19 19:15:09 +0000

    Don't require a service to be specified immediately

--- commands/setup.rb
+++ commands/setup.rb
@@ -3,19 +3,17 @@ require 'rest_client'
 
 class LighthouseAgent
   desc :setup, "Register this host with the management server"
-  method_option :service, required: true
+  method_option :service, required: false
   method_option :environment, default: "development"
 
   def setup(url)
     params = {
-      host: {
         hostname: Socket.gethostname,
         service_slug: options.service,
         environment: options.environment
-      }
     }
 
-    RestClient.post "#{url}/hosts/", params.to_json, content_type: :json
+    RestClient.post "#{url}/hosts/", { host: params }.to_json, content_type: :json
     Configuration.new(management_server: url).save
   end
 end
