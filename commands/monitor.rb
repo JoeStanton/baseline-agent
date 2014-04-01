d05978d7d402b7dbaed5424578cfcee1ba1a18f0
Joe Stanton <joe.stanton@red-badger.com>
2014-04-01 11:38:28 +0100

    Shorten the monitor commands

--- commands/monitor.rb
+++ commands/monitor.rb
@@ -4,22 +4,22 @@ require_relative '../lib/monitor'
 Thread.abort_on_exception=true
 
 class LighthouseAgent
-  desc :start_monitor, "Continuously monitor the specified service"
-  def start_monitor(system)
+  desc :start, "Continuously monitor the specified service"
+  def start(system)
     return puts 'Must run as root'.red unless Process.uid == 0
     system = load_system(system)
     Dante::Runner.new(process_name(system)).execute(daemonize: true) { Monitor.start!(system) }
   end
 
-  desc :stop_monitor, "Stop monitoring the specified service"
-  def stop_monitor(system)
+  desc :stop, "Stop monitoring the specified service"
+  def stop(system)
     return puts 'Must run as root'.red unless Process.uid == 0
     system = load_system(system)
     Dante::Runner.new(process_name(system)).execute(kill: true)
   end
-  
-  desc :restart_monitor, "Restart monitoring the specified service"
-  def restart_monitor(system)
+
+  desc :restart, "Restart monitoring the specified service"
+  def restart(system)
     return puts 'Must run as root'.red unless Process.uid == 0
     stop_monitor(system)
     start_monitor(system)
