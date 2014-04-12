ff65569d916bc3d6230aa42f0e42465c5a06a09a
Joe Stanton <joe.stanton@red-badger.com>
2014-04-12 11:57:47 +0100

    Add "or use sudo"

--- commands/monitor.rb
+++ commands/monitor.rb
@@ -6,21 +6,21 @@ Thread.abort_on_exception=true
 class BaselineAgent
   desc :start, "Continuously monitor the specified service"
   def start(system)
-    return puts 'Must run as root'.red unless Process.uid == 0
+    return puts 'Must run as root (or use sudo)'.red unless Process.uid == 0
     system = load_system(system)
     Dante::Runner.new(process_name(system)).execute(daemonize: true) { Monitor.start!(system) }
   end
 
   desc :stop, "Stop monitoring the specified service"
   def stop(system)
-    return puts 'Must run as root'.red unless Process.uid == 0
+    return puts 'Must run as root (or use sudo)'.red unless Process.uid == 0
     system = load_system(system)
     Dante::Runner.new(process_name(system)).execute(kill: true)
   end
 
   desc :restart, "Restart monitoring the specified service"
   def restart(system)
-    return puts 'Must run as root'.red unless Process.uid == 0
+    return puts 'Must run as root (or use sudo)'.red unless Process.uid == 0
     stop(system)
     start_monitor(system)
   end
