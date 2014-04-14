f3ace85974663ec62de0f8e2ae60b1b8157cc7f3
Joe Stanton <joe.stanton@red-badger.com>
2014-04-14 23:08:33 +0100

    Execute cukes as part of build

--- commands/discover.rb
+++ commands/discover.rb
@@ -3,10 +3,8 @@ require_relative '../lib/service_spec'
 class BaselineAgent
   desc :discover, "Creates a service definition from detected system services"
 
-  def discover
-    name = ask "Please enter a service name:"
-    error "you must enter a service name" unless name
-
+  def discover(name)
+    error "Please specify a name" unless name
     filename = "#{name.downcase.gsub(' ', '_').gsub(/[^0-9A-Za-z_]/, '')}.rb"
 
     content = ServiceSpec.new(name).detect!.render
--- features/discover.feature
+++ features/discover.feature
@@ -1,8 +1,6 @@
 Feature: baseline-agent discover
   Scenario: Service Discovery
-    When I run `baseline-agent discover` interactively
-    And I type "example service"
-    And I type ""
+    When I run `baseline-agent discover "example service"`
     Then the exit status should be 0
     Then a file named "example_service.rb" should exist
     Then the file "example_service.rb" should contain "service 'example service'"
